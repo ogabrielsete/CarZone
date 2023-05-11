154e46b7b174863882a860715873b048a52c3060
ogabrielsete <biel-sr@hotmail.com>
2023-05-11 15:28:46 -0300

    Criação de páginas de Listagem de Carros com a função de Criar, Editar e Deletar

---
+++ Controllers/VeiculoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarZone.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/VeiculoModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CarZone.Models
+{
+    public class VeiculoModel
+    {
+        public int Id { get; set; }
+
+        // colocar modelo que é uma fk
+
+        public int Placa { get; set; }
+
+        public int Ano { get; set; }
+
+        public bool StatusVenda { get; set; }
+    }
+}
