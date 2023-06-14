57e4dff4f3caf1c69a6f3c7ab7f65c4276b17670
ogabrielsete <biel-sr@hotmail.com>
2023-06-14 18:57:52 -0300

    Integração com API de CEP para preenchimento automatico do endereço do cliente

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -1,6 +1,13 @@
 ﻿using CarZone.Models;
 using CarZone.Repositorio;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using static CarZone.Controllers.ClientesController;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace CarZone.Controllers
 {
@@ -43,6 +50,12 @@ namespace CarZone.Controllers
         [HttpPost]
         public IActionResult Criar(Cliente cliente)
         {
+            if (!ValidarCPF(cliente.CPF))
+            {
+                ModelState.AddModelError("CPF", "CPF inválido");
+                return View("CadastrarCliente", cliente);
+            }
+
             _clienteRepositorio.Adicionar(cliente);
             return RedirectToAction("Index");
         }
@@ -50,11 +63,54 @@ namespace CarZone.Controllers
         [HttpPost]
         public IActionResult Alterar(Cliente cliente)
         {
+            if (!ValidarCPF(cliente.CPF))
+            {
+                ModelState.AddModelError("CPF", "CPF inválido");
+                return View("CadastrarCliente", cliente);
+            }
             _clienteRepositorio.Atualizar(cliente);
             return RedirectToAction("Index");
         }
 
 
+        public bool ValidarCPF(string cpf)
+        {
+            cpf = cpf.Replace(".", "").Replace("-", ""); // Remove os caracteres especiais do CPF
 
+            // Verifica se o CPF tem 11 dígitos
+            if (cpf.Length != 11)
+                return false;
+
+            // Verifica se todos os dígitos são iguais (caso contrário, é um CPF inválido)
+            if (new string(cpf[0], 11) == cpf)
+                return false;
+
+            // Calcula o primeiro dígito verificador
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+
+            int resto = soma % 11;
+            int digitoVerificador1 = (resto < 2) ? 0 : 11 - resto;
+
+            // Verifica se o primeiro dígito verificador está correto
+            if (int.Parse(cpf[9].ToString()) != digitoVerificador1)
+                return false;
+
+            // Calcula o segundo dígito verificador
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+
+            resto = soma % 11;
+            int digitoVerificador2 = (resto < 2) ? 0 : 11 - resto;
+
+            // Verifica se o segundo dígito verificador está correto
+            if (int.Parse(cpf[10].ToString()) != digitoVerificador2)
+                return false;
+
+            return true; // CPF válido
+        }
     }
 }
+
